c06cc1b2f844b3d34ab5899b5b91798ca1e5ac59
kedz <kedzie@cs.columbia.edu>
2015-08-28 16:57:06 -0400

    Added setup.py for cython compile and install.

---
+++ python/setup.py
@@ -0,0 +1,82 @@
+from setuptools import setup, Extension
+import os
+
+def find_nlp_sources():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    c_src = os.path.join(
+        os.path.split(cwd)[0],
+        "c", "src")
+    if not os.path.exists(c_src):
+        raise Exception("Could not find c sources.")
+
+    sources = [os.path.join(c_src, f) 
+               for f in os.listdir(c_src) if f.endswith(".c")]
+    return sources, c_src
+
+def build_extension():
+
+    try:
+        from Cython.Build import cythonize
+        use_cython = True
+        ext = ".pyx"
+    except ImportError, e:
+        use_cython = False
+        ext = ".c"
+
+    sources_nlp, include_nlp = find_nlp_sources()
+
+    sources_cyt = [os.path.join('nlp', 'ez{}'.format(ext)),]
+#              #]
+#    libraries = ["fnlp"]#['oolm', 'dstruct', 'misc', 'z', 'gomp']
+#
+#    _mem_mgr = Extension(
+#        "nlp._mem_mgr",
+#        sources=[os.path.join('nlp', '_mem_mgr{}'.format(ext)),],
+#        libraries=libraries,
+#        extra_compile_args=["-I/home/kedz/projects2015/fnlp/fnlp-c/include", "-O4"],
+#        extra_link_args=["-L/home/kedz/projects2015/fnlp/fnlp-c/lib"],
+#        language="c")
+# 
+#
+#    config = Extension(
+#        "nlp.config",
+#        sources=[os.path.join('nlp', 'config{}'.format(ext)),],
+#        libraries=libraries,
+#        extra_compile_args=["-I/home/kedz/projects2015/fnlp/fnlp-c/include", "-O4"],
+#        extra_link_args=["-L/home/kedz/projects2015/fnlp/fnlp-c/lib"],
+#        language="c")
+#       
+    ez = Extension(
+        "nlp.ez",
+        sources=[os.path.join('nlp', 'ez{}'.format(ext))] + sources_nlp,
+#        libraries=libraries,
+        extra_compile_args=["-I{}".format(include_nlp), "-O9", "-std=c11" ],
+        extra_link_args=["-O9"],
+        language="c")
+#
+
+    if use_cython is True:
+        return cythonize([ez])
+    else:
+        return [ez]
+
+
+    if use_cython is True:
+        return cythonize([_mem_mgr, config, ez])
+    else:
+        return [_mem_mgr, config, ez]
+
+setup(
+    name="nlp",
+    version="0.0.1",
+    author="Chris Kedzie",
+    author_email="kedzie@cs.columbia.edu",
+    description="A package for fast NLP algorithms.",
+#    license="",
+#    keywords="",
+#    url="",
+    packages=['nlp'],
+#    long_description="",
+#    classifiers=[],
+    ext_modules=build_extension(),
+)
