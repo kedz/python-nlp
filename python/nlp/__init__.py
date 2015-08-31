81addba8306e9bcd956907001eb8f1606bad6d8c
kedz <kedzie@cs.columbia.edu>
2015-08-31 02:36:50 -0400

    Added getter for global PTB tokenizer config.

--- python/nlp/__init__.py
+++ python/nlp/__init__.py
@@ -1,5 +1,6 @@
+from nlp.libnlp import get_global_PTB_config
 from nlp.ez import tokenize
 
 """This module does natural language processing."""
 
-__all__ = ["tokenize"]
+__all__ = ["tokenize", "get_global_PTB_config"]
