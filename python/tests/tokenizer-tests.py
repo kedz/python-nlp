cb6ad19e8d8a22e6a4b750529591458c5d58db05
kedz <kedzie@cs.columbia.edu>
2015-09-02 03:05:55 -0400

    Added unicode quote norming tests. Currently failing.

--- python/tests/tokenizer-tests.py
+++ python/tests/tokenizer-tests.py
@@ -89,3 +89,64 @@ def spmdash_NO_normalize_test():
     for token, gold_token in zip(tokens, gold):
         assert token == gold_token.encode("utf-8")
     assert len(tokens) == len(gold)
+
+def spamp_normalize_test():
+
+    nlp.get_global_PTB_config().normalize_amp = True
+    string = u"&amp; is not normed yet";
+    gold = [u"&", u"is", u"not", u"normed", u"yet"]
+    tokens = nlp.tokenize(string)
+    print tokens
+    for token, gold_token in zip(tokens, gold):
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+ 
+def spamp_NO_normalize_test():
+
+    nlp.get_global_PTB_config().normalize_amp = False
+    string = u"&amp; is not normed yet";
+    gold = [u"&amp;", u"is", u"not", u"normed", u"yet"]
+    tokens = nlp.tokenize(string)
+    print tokens
+    for token, gold_token in zip(tokens, gold):
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def sppunc_test():
+    string = u"&HT; &TL; &UR; &LR; &QC; &QL; &QR; &odq; &cdq; &#0123;" 
+    gold = [u"&HT;", u"&TL;", u"&UR;", u"&LR;", u"&QC;", u"&QL;", u"&QR;",
+            u"&odq;", u"&cdq;", u"&#0123;"] 
+    tokens = nlp.tokenize(string)
+    print tokens
+    for token, gold_token in zip(tokens, gold):
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def word_redaux_test_NO_normalize_quotes():
+
+    nlp.get_global_PTB_config().normalize_quotes = None
+    string = u"Ma'm you're I'll Chris's I'd you're"
+    gold = [u"Ma", u"'m", u"you", u"'re", u"I", u"'ll", u"Chris", u"'s", u"I",
+            u"'d", u"you", u"'re"]
+    tokens = nlp.tokenize(string)
+    print tokens
+    for token in tokens:
+        print token.decode("utf-8"),
+    print
+    for token, gold_token in zip(tokens, gold):
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def word_redaux_test_UNICODE_normalize_quotes():
+    nlp.get_global_PTB_config().normalize_quotes = u"unicode"
+    string = u"Ma\u0092m you\u2019re I&apos;ll Chris's I'd you're"
+    gold = [u"Ma", u"\u2019m", u"you", u"\u2019re", u"I", u"\u2019ll", u"Chris", u"\u2019s", u"I",
+            u"\u2019d", u"you", u"\u2019re"]
+    tokens = nlp.tokenize(string)
+    print tokens
+    for token in tokens:
+        print token.decode("utf-8"),
+    print
+    for token, gold_token in zip(tokens, gold):
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
