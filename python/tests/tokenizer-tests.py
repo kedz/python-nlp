e038f01f814e84ff9d438027f9825dac39e29266
kedz <kedzie@cs.columbia.edu>
2015-09-05 22:36:09 -0400

    Added more tests to tokenizer. 3 are currently not passing.

--- python/tests/tokenizer-tests.py
+++ python/tests/tokenizer-tests.py
@@ -295,14 +295,71 @@ def apoword2_test():
         assert token == gold_token.encode("utf-8")
     assert len(tokens) == len(gold)
 
-def full_or_likely_url_NO_escape_test():
-    string = u"https://cs.columbia.edu http://www.google.com " + \
-             u"www.nytimes.com/world.html nytimes.com nytimes.com/americas"
-    gold = [u"https://cs.columbia.edu", 
-            u"http://www.google.com",
-            u"www.nytimes.com/world.html",
-            u"nytimes.com", 
-            u"nytimes.com/americas",]
+#def full_or_likely_url_NO_escape_test():
+#    string = u"https://cs.columbia.edu http://www.google.com " + \
+#             u"www.nytimes.com/world.html nytimes.com nytimes.com/americas"
+#    gold = [u"https://cs.columbia.edu", 
+#            u"http://www.google.com",
+#            u"www.nytimes.com/world.html",
+#            u"nytimes.com", 
+#            u"nytimes.com/americas",]
+#
+#    tokens = nlp.tokenize(string)
+#    for token, gold_token in zip(tokens, gold):
+#        print token, gold_token.encode("utf-8")
+#        assert token == gold_token.encode("utf-8")
+#    assert len(tokens) == len(gold)
+
+#def email_test():
+#    string = u"<crk2130@columbia.edu> thekedz@gmail.com " + \
+#             u"christopher.r.kedzie@gmail.com &lt;chris.kedzie@outlook.com&gt;"
+#    gold = [u"<crk2130@columbia.edu>", u"thekedz@gmail.com", 
+#            u"christopher.r.kedzie@gmail.com", 
+#            u"&lt;chris.kedzie@outlook.com&gt;"]
+#
+#    tokens = nlp.tokenize(string)
+#    print tokens
+#    for token, gold_token in zip(tokens, gold):
+#        print token, gold_token.encode("utf-8")
+#        assert token == gold_token.encode("utf-8")
+#    assert len(tokens) == len(gold)
+#    print nlp.tokenize("ked zie@gmail.com")
+#    print nlp.tokenize(u"ked\u00A0zie@gmail.com".encode("utf-8"))
+
+def twiiter_test():
+    string = u"@the_real_kedzie1 #nlp #cs"
+    gold = [u"@the_real_kedzie1",  u"#nlp", u"#cs"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def date_test_NO_escapeing():
+    string = u"01/29/1986 04-11-1995"
+    gold = [u"01/29/1986", u"04-11-1995"]
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def number_test():
+    string = u"-1234 +1\u00AD200.4 +12.98 -.33 +.02 +0.2"
+    gold = [u"-1234", u"+1200.4", u"+12.98", u"-.33", u"+.02", u"+0.2"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def subsupnum_test():
+    string = u"note\u207A\u2070\u00B9\u00B2\u00B3 \u2074 \u2075 \u2076" \
+        + u" \u2080\u2081 \u2082 \u2083"
+    gold = [u"note", u"\u207A\u2070\u00B9\u00B2\u00B3", u"\u2074", u"\u2075",
+            u"\u2076", u"\u2080\u2081", u"\u2082", u"\u2083"]
 
     tokens = nlp.tokenize(string)
     for token, gold_token in zip(tokens, gold):
@@ -311,3 +368,192 @@ def full_or_likely_url_NO_escape_test():
     assert len(tokens) == len(gold)
 
 
+# do fracs
+
+
+def tbspec_test():
+    string = u"-RRB- -LRB- -RCB- -lcb- -lsb- C.D.s PRO-nouns anti-con " + \
+        u"s&p-500  S&amp;p-500  S&amp;Ls  cap'n C'est" #
+    gold = [u"-RRB-", u"-LRB-", u"-RCB-", u"-lcb-", u"-lsb-", u"C.D.s", 
+            u"PRO-", u"nouns", u"anti-", u"con", u"s&p-500", u"S&p-500", 
+            u"S&Ls", u"cap'n", u"C'est"]
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def bangwords_test():
+    string = u"E! yahoo! Jeopardy! ok!\nmagazine"
+    gold = [u"E!", u"yahoo!", u"Jeopardy!", u"ok!", u"magazine"]
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+# do thing3 here
+def thing3_test_NO_escaping():
+    assert 0
+
+def dolsign_test():
+    string = u"US$ $ # UK$"
+    gold = [u"US$", u"$", u"#", u"UK$"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+
+def dolsign2_test_NO_normalize():
+    string = u"\u00A2 \u00A3 \u00A4 \u00A5 \u0080 \u20A0 \u20AC " + \
+        u"\u060B \u0E3F \u20A4 \uFFE0 \uFFE1 \uFFE5 \uFFE6"
+    gold = [u"\u00A2", u"\u00A3", u"\u00A4", u"\u00A5", u"\u0080", u"\u20A0",
+            u"\u20AC", u"\u060B", u"\u0E3F", u"\u20A4", u"\uFFE0", u"\uFFE1",
+            u"\uFFE5", u"\uFFE6"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+
+
+def dolsign2_test_normalize():
+    string = u"\u00A2 \u00A3 \u00A4 \u00A5 \u0080 \u20A0 \u20AC " + \
+        u"\u060B \u0E3F \u20A4 \uFFE0 \uFFE1 \uFFE5 \uFFE6"
+    gold = [u"cents", u"#", u"\\$", u"\u00A5", u"\\$", u"\\$", u"\\$", 
+            u"\u060B", u"\u0E3F", u"\u20A4", u"\uFFE0", u"\uFFE1", u"\uFFE5",
+            u"\uFFE6"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+
+
+#  private static final Pattern CENTS_PATTERN = Pattern.compile("\u00A2");
+#  private static final Pattern POUND_PATTERN = Pattern.compile("\u00A3");
+#  private static final Pattern GENERIC_CURRENCY_PATTERN = Pattern.compile("[\u0080\u00A4\u20A0\u20AC]");
+
+#  private static String normalizeCurrency(String in) {
+#    String s1 = in;
+#    s1 = CENTS_PATTERN.matcher(s1).replaceAll("cents");
+#    s1 = POUND_PATTERN.matcher(s1).replaceAll("#");  // historically used for pound in PTB3
+#    s1 = GENERIC_CURRENCY_PATTERN.matcher(s1).replaceAll("\\$");  // Euro (ECU, generic currency)  -- no good translation!
+#    return s1;
+#  }
+
+
+def terminal_redaux_test():
+    string = u"test ends with you're"
+    gold = [u"test", u"ends", u"with", u"you", u"'re"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def terminal_sredaux_test():
+    string = u"test ends with wouldn't"
+    gold = [u"test", u"ends", u"with", u"would", u"n't"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+
+def quotes_test():
+    raise Exception("IMPLEMENT ME!!!")
+
+def test_fake_duckfeet():
+    string = u"duck<<feet >>"
+    gold = [u"duck", u"<<", u"feet", u">>"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def test_misc_symbols():
+    def rngstr(uni1, uni2):
+        return u" ".join(unichr(i) for i in xrange(ord(uni1), ord(uni2) + 1))
+    string = u" ".join(
+        [
+            u"+ % & ~ ^ | \\ \u00A6 \u00A7 \u00A8 \u00A9 \u00AC \u00AE \u00AF",
+            rngstr(u"\u00B0", u"\u00B3"),
+            rngstr(u"\u00B4", u"\u00BA"),
+            u"\u00D7 \u00F7 \u0387 \u05BE \u05C0 \u05C3 \u05C6 \u05F3 \u05F4",
+            rngstr(u"\u0600", u"\u0603"),
+            rngstr(u"\u0606", u"\u060A"),
+            u"\u060C \u0614 \u061B \u061E \u066A \u066D",
+            rngstr(u"\u0703", u"\u070D"),
+            u"\u07F6 \u07F7 \u07F8 \u0964 \u0965 \u0E4F \u1FBD \u2016 \u2017",
+            rngstr(u"\u2020", u"\u2023"),
+            rngstr(u"\u2030", u"\u2038"),
+            u"\u203B \u2043",
+            rngstr(u"\u203E", u"\u2042"),
+            u"\u2044",
+            rngstr(u"\u207A", u"\u207F"),
+            rngstr(u"\u208A", u"\u208E"),
+            rngstr(u"\u2100", u"\u214F"),
+            rngstr(u"\u2190", u"\u21FF"),
+            rngstr(u"\u2200", u"\u2BFF"),
+            u"\u3012 \u30FB",
+            rngstr(u"\uFF01", u"\uFF0F"),
+            rngstr(u"\uFF1A", u"\uFF20"),
+            rngstr(u"\uFF3B", u"\uFF40"),
+            rngstr(u"\uFF5B", u"\uFF65"),
+            u"\uFF65"
+        ])
+    gold = string.split(u" ")
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def cp1252_bullet_tm_test():
+    string = u"\x99\x95"
+    gold = [u"\u2122", u"\u2022"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+
+def test_spaces():
+    string = u"\u200Bthis\u200Eis\u200Fa\uFEFFtest\tfor&nbsp;\u00A0" + \
+        u"\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008" + \
+        u"\u2009\u200A\u3000sure"
+    gold = [u"this", u"is", u"a", u"test", u"for", u"sure"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
+
+def tokenize_nl_test():
+    nlp.get_global_PTB_config().tokenize_newlines = True
+    string = u"\n word \r\n \r \u2028 \u2029 \u000B \u000C \u0085"
+    gold = [u"*NL*", u"word", u"*NL*", u"*NL*", u"*NL*", u"*NL*", u"*NL*",
+            u"*NL*", u"*NL*"]
+
+    tokens = nlp.tokenize(string)
+    for token, gold_token in zip(tokens, gold):
+        print token, gold_token.encode("utf-8")
+        assert token == gold_token.encode("utf-8")
+    assert len(tokens) == len(gold)
