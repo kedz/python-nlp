ee8fb262479cb4d1facc16653fdfa3b399909e99
kedz <kedzie@cs.columbia.edu>
2015-09-19 16:57:40 -0400

    Major rework of the memory manager. Fixed a bad bug in the deallocation of variably sized objects in a managed list of objects. sheesh.

--- c/src/mem_manager.c
+++ c/src/mem_manager.c
@@ -14,7 +14,7 @@ inline NL_v_memmgr *NL_new_v_memmgr(size_t block_size) {
         size_t size = 1;
         for (int i=0; i < manager->max_pools; i++) {
             size = size * 2;
-            manager->pools[i] = NL_new_memmgr(block_size, size);
+            manager->pools[i] = NL_new_memmgr(block_size, size, i);
         }
 
     }
@@ -55,42 +55,56 @@ inline void *NL_allocate_mem_size(NL_v_memmgr *mgr, size_t object_size) {
         size_t size = mgr->pools[mgr->max_pools-1]->object_size;
         for (int i=mgr->max_pools; i < bucket + 1; i++) {
             size = size * 2;
-            mgr->pools[i] = NL_new_memmgr(mgr->block_size, size);
+            mgr->pools[i] = NL_new_memmgr(mgr->block_size, size, i);
         }
         mgr->max_pools = bucket + 1;
     }
 
     if (bucket > 0 && mgr->pools[bucket-1]->object_size == object_size)
         bucket--;
+#ifdef NLDBG
+    printf("NL_v_memmgr allocating chunk of size %lu bytes for an object of "
+            "size %lu\n", mgr->pools[bucket]->object_size, object_size);
+#endif 
     return NL_allocate_mem(mgr->pools[bucket]);
 
 }
 
 inline void *NL_allocate_16_mem(NL_v_memmgr *mgr) {
+
+#ifdef NLDBG
+    printf("NL_v_memmgr allocating chunk of size %lu bytes for an object of "
+            "size 16 bytes\n", mgr->pools[3]->object_size);
+#endif
     return NL_allocate_mem(mgr->pools[3]);
 }
 
 inline void *NL_allocate_32_mem(NL_v_memmgr *mgr) {
+#ifdef NLDBG
+    printf("NL_v_memmgr allocating chunk of size %lu bytes for an object of "
+            "size 32 bytes\n", mgr->pools[4]->object_size);
+#endif
     return NL_allocate_mem(mgr->pools[4]);
 }
 
 inline void NL_deallocate_v_mem(NL_v_memmgr *mgr, void **data) {
-
     if ( *data == NULL)
         return;
 
-    size_t object_size;
-    for (int i=0; i < mgr->max_pools; i++) {
-        object_size = mgr->pools[i]->object_size;
-        unsigned char * guard_byte = ((unsigned char *) *data) + object_size;
-        if (*guard_byte == 0xDE && *(guard_byte + 1) == 0xAD) {
-            return NL_deallocate_mem(mgr->pools[i], data);   
-        }
+    unsigned char *buf = *data;
+    unsigned char id_byte = *(&buf[0] - 1);
+
+    if (id_byte < mgr->max_pools) {
+        #ifdef NLDBG
+        printf("Deallocating memory of size %lu bytes.\n",
+            mgr->pools[id_byte]->object_size);
+        #endif
+        return NL_deallocate_mem(mgr->pools[id_byte], data);   
     }
 
     fprintf(stderr, 
         "Warning: deallocation of variably sized memory failed.\n"
-        "Couldn't find guard bytes.\n");
+        "ID byte points to a pool that doesn't exist.\n");
 
     return;
 }
@@ -108,18 +122,21 @@ inline void NL_free_v_memmgr(NL_v_memmgr ** mgr) {
     }
 }
 
-inline NL_memmgr *NL_new_memmgr(size_t block_size, size_t object_size) {
+inline NL_memmgr *NL_new_memmgr(size_t block_size, size_t object_size,
+        unsigned char id_byte) {
     NL_memmgr *m = malloc(sizeof(NL_memmgr));
     if (m != NULL) {
         m->block_size = block_size;
         m->object_size = object_size;
-        m->actual_block_size = (object_size + 2) * block_size;
-        m->actual_object_size = object_size + 2;
+        m->actual_block_size = (object_size + 1) * block_size;
+        m->actual_object_size = object_size + 1;
         m->available = 0;
         m->free_list = NULL;
         m->mem_list = NULL;
         m->empty_containers = NULL;
         m->allocs = 0;
+        m->id_byte = id_byte;
+        
     }
 
     return m;
@@ -137,15 +154,11 @@ inline void __NL_allocate_block(NL_memmgr *mgr) {
     }
 
     block_container->data = block;
-    size_t guard_start = mgr->object_size; 
-    
 
     for (int i=0; i < mgr->actual_block_size; i+=mgr->actual_object_size) {
 
-        unsigned char *guard_byte = &((unsigned char *)block)[i] + guard_start;
-        *guard_byte = 0xde;
-        guard_byte++; 
-        *guard_byte = 0xad;   //end of block
+        unsigned char *guard_byte = &((unsigned char *)block)[i];
+        *guard_byte = mgr->id_byte;
         NL_mem_list *object_container = malloc(sizeof(NL_mem_list));
 
         if (object_container == NULL) {
@@ -176,14 +189,34 @@ inline void *NL_allocate_mem(NL_memmgr *mgr) {
     mgr->available -= 1;
     mgr->allocs++;
 
-    return (void *) container->data;
+    return (void *) &container->data[1];
 }
 
 inline void NL_deallocate_mem(NL_memmgr *mgr, void **data) {
+    if (*data == NULL) {
+        #ifdef NLDBG 
+        printf("Deallocater pass null pointer. Doing nothing.\n");
+        #endif
+
+        return;
+
+    }
+    unsigned char *buf = *data;
+    unsigned char *id_byte = (&buf[0] - 1);
+
+    if (*id_byte != mgr->id_byte) {// ||  
+            //(unsigned char) *(*(*data[0]-1)) != mgr->id_byte) {
+        #ifdef NLDBG 
+        printf("Deallocater found bad id byte. Doing nothing.\n");
+        #endif
+        return; 
+    }
+
+
     NL_mem_list *container = mgr->empty_containers;
     mgr->empty_containers = container->next;
      
-    container->data = *data;
+    container->data = id_byte;
     *data = NULL;
     container->next = mgr->free_list;
     mgr->free_list = container;
--- c/src/mem_manager.h
+++ c/src/mem_manager.h
@@ -20,7 +20,7 @@ typedef struct NL_memmgr {
     NL_mem_list *mem_list;
     NL_mem_list *empty_containers;
     unsigned int allocs;
-    
+    unsigned char id_byte;
 } NL_memmgr;
 
 typedef struct NL_v_memmgr {
@@ -38,12 +38,15 @@ void NL_deallocate_v_mem(NL_v_memmgr *mgr, void **data);
 void *NL_allocate_16_mem(NL_v_memmgr *mgr); // test these and edge cases 0 and too big
 void *NL_allocate_32_mem(NL_v_memmgr *mgr);
 
-NL_memmgr *NL_new_memmgr(size_t block_size, size_t object_size);
+NL_memmgr *NL_new_memmgr(size_t block_size, size_t object_size, 
+    unsigned char id_byte);
 void NL_free_memmgr(NL_memmgr **m_ptr);
 
 void *NL_allocate_mem(NL_memmgr *mgr);
 void NL_deallocate_mem(NL_memmgr *mgr, void **data);
 
+void __NL_allocate_block(NL_memmgr *mgr);
+void __NL_release_pool(NL_memmgr *mgr);
 #define NL_V_MEMMGR_INIT_POOL_SIZE 8
 
 #endif
