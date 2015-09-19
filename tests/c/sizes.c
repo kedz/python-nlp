0f503fe8ee828dfb0924089162a9975b817d69b5
kedz <kedzie@cs.columbia.edu>
2015-09-19 17:06:22 -0400

    Prints more sizes of things. TODO: Make this a test.

--- tests/c/sizes.c
+++ tests/c/sizes.c
@@ -1,4 +1,5 @@
 #include "nlp/document.h"
+#include "nlp/tokenizer.h"
 #include <stddef.h>
 #include <stdio.h>
 
@@ -8,8 +9,10 @@ int main() {
     printf("unsigned char*: %zu\n", sizeof(unsigned char*));
     printf("size_t: %zu\n", sizeof(size_t));
     printf("NL_span size: %zu\n", sizeof(NL_span));
+    printf("NL_span *: %zu\n", sizeof(NL_span *)); 
+    printf("NL_span **: %zu\n", sizeof(NL_span **)); 
     printf("NL_document size: %zu\n", sizeof(NL_document));
-    
+    printf("__token_list: %zu\n", sizeof(__token_list));
 
     return 0;
 }
