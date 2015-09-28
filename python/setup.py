941eb417ee5f703a1c8cb61c09fff33c40916e5a
kedz <kedzie@cs.columbia.edu>
2015-09-28 01:48:24 -0400

    Cleaned up setup.py.

--- python/setup.py
+++ python/setup.py
@@ -1,18 +1,6 @@
 from setuptools import setup, Extension
 import os
 
-def find_nlp_sources():
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    c_src = os.path.join(
-        os.path.split(cwd)[0],
-        "c", "src")
-    if not os.path.exists(c_src):
-        raise Exception("Could not find c sources.")
-
-    sources = [os.path.join(c_src, f) 
-               for f in os.listdir(c_src) if f.endswith(".c")]
-    return sources, c_src
-
 def build_extension():
 
     try:
@@ -23,59 +11,38 @@ def build_extension():
         use_cython = False
         ext = ".c"
 
-    sources_nlp, include_nlp = find_nlp_sources()
+    include_nlp = "../include/"
+    lib_nlp = "../lib/"
 
-#    libraries = ["fnlp"]#['oolm', 'dstruct', 'misc', 'z', 'gomp']
-#
-#    _mem_mgr = Extension(
-#        "nlp._mem_mgr",
-#        sources=[os.path.join('nlp', '_mem_mgr{}'.format(ext)),],
-#        libraries=libraries,
-#        extra_compile_args=["-I/home/kedz/projects2015/fnlp/fnlp-c/include", "-O4"],
-#        extra_link_args=["-L/home/kedz/projects2015/fnlp/fnlp-c/lib"],
-#        language="c")
-# 
-#
-#    config = Extension(
-#        "nlp.config",
-#        sources=[os.path.join('nlp', 'config{}'.format(ext)),],
-#        libraries=libraries,
-#        extra_compile_args=["-I/home/kedz/projects2015/fnlp/fnlp-c/include", "-O4"],
-#        extra_link_args=["-L/home/kedz/projects2015/fnlp/fnlp-c/lib"],
-#        language="c")
-#       
     libnlp = Extension(
         "nlp.libnlp",
-        sources=[os.path.join("nlp", "libnlp{}".format(ext)),
-            #"nlp/libnlp.pxd"
-            ] + sources_nlp,
-        include_dirs=["."],
-        extra_compile_args=["-I{}".format(include_nlp), "-O9", "-std=c11" ],
+        sources=[
+            os.path.join("nlp", "libnlp{}".format(ext)),
+        ],
+        include_dirs=[".", "{}".format(include_nlp)],
+        library_dirs=[".", "{}".format(lib_nlp)],
+        libraries = ["nlp"],
+        extra_compile_args=["-O9", "-std=c11" ],
         extra_link_args=["-O9"],
         language="c")
-#
+
     ez = Extension(
         "nlp.ez",
-        sources=[os.path.join('nlp', 'ez{}'.format(ext)), 
-            "nlp/libnlp.pyx",
-            #"nlp/libnlp.pxd"
-            ] + sources_nlp,
-        include_dirs=["."],
-        extra_compile_args=["-I{}".format(include_nlp), "-O9", "-std=c11" ],
+        sources=[
+            os.path.join('nlp', 'ez{}'.format(ext)), 
+            "nlp/libnlp{}".format(ext),
+        ],
+        libraries = ["nlp"],
+        include_dirs=[".", "{}".format(include_nlp)],
+        library_dirs=[".", "{}".format(lib_nlp)],
+        extra_compile_args=["-O9", "-std=c11" ],
         extra_link_args=["-O9"],
         language="c")
-#
 
     if use_cython is True:
-        return cythonize([libnlp, ez])
+        return cythonize([libnlp, ez], gdb_debug=True)
     else:
         return [libnlp, ez]
-
-
-    if use_cython is True:
-        return cythonize([_mem_mgr, config, ez])
-    else:
-        return [_mem_mgr, config, ez]
 
 setup(
     name="nlp",
