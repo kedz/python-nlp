a2482829001e3b69a4c09920c02384cd2ca633ab
kedz <kedzie@cs.columbia.edu>
2015-09-28 01:41:04 -0400

    Removed pipeline for now.

--- python/nlp/__init__.py
+++ python/nlp/__init__.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import, division, print_function, \
         unicode_literals
 
 from nlp.libnlp import get_global_PTB_config
-from nlp.ez import tokenize
-import nlp.pipeline as pipeline
-from nlp.pipeline import Pipeline
+from nlp.ez import tokenize, sent_tokenize
+#import nlp.pipeline as pipeline
+#from nlp.pipeline import Pipeline
 
 """This module does natural language processing."""
 
-__all__ = ["tokenize", "get_global_PTB_config", "pipeline", "Pipeline"]
+__all__ = ["tokenize", "sent_tokenize", "get_global_PTB_config"]
